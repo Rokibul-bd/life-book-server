699459859369400e4fa6128de802b4fa67d7bb15
Rokibul-bd <mdrokbiulislamcmt176@gmail.com>
2022-12-29 02:13:58 +0600

    user route create

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const userCollections = client.db('lifeBook').collection('users');
 const postCollections = client.db('lifeBook').collection('post');
-
+const commentsCollections = client.db('lifeBook').collection('comments');
 async function run() {
     try {
         app.get('/', async (req, res) => {
@@ -28,6 +28,12 @@ async function run() {
             const user = req.body;
             const result = await userCollections.insertOne(user)
             res.send(result)
+        })
+        app.get('/user/:email', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const result = await userCollections.find(query).toArray()
+            res.send(result);
         })
         app.post('/post', async (req, res) => {
             const post = req.body;
@@ -40,7 +46,11 @@ async function run() {
             const posts = await postCollections.find(query).toArray();
             res.send(posts)
         })
-
+        app.post('/comment', async (req, res) => {
+            const comment = req.body;
+            const result = await commentsCollections.insertOne(comment);
+            res.send(result)
+        })
 
     }
     finally {
